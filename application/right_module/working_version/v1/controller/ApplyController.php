6823256b75d0dd507d57379c918d5bcecddcb004
时广玉 <shiguangyu@dlaotianhuang.com>
2018-07-18 11:15:41 +0800

    2018/07/18 10:31 更新项目开发权限管理模块

--- application/right_module/working_version/v1/controller/ApplyController.php
+++ application/right_module/working_version/v1/controller/ApplyController.php
@@ -55,11 +55,10 @@ class ApplyController extends Controller
      */
     public function applyRegister(Request $request)
     {
-        return "<h1>:{$request->get('code')}<h1>";
         // 通过code换取网页授权access_token显示首页
         $array = (new LoginLibrary())->loginLibrary($request->get('code'));
         // 验证token值
-        if($array['msg']=='error') return "<h1>:{$request->get('code')}<h1>";
+        if($array['msg']=='error') return "<h1>{$array['data']}<h1>";
         // 获取用户申请管理员操作页面地址
         $url = config('qd_html_url.HTTP_URL');
         $url.= config('qd_html_url.Admin_Register');
