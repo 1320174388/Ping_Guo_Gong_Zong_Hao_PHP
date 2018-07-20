a6c49aaac1490a59cb3bc85f53219bda9b270bae
时广玉 <shiguangyu@dlaotianhuang.com>
2018-07-20 21:01:26 +0800

    2018/07/20 21:01 更新项目-开发公众号后台登录接口

---
+++ application/page_module/working_version/v1/controller/PageController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  PageController.php
+ *  创 建 者 :  Shi Guang Yu
+ *  创建日期 :  2018/07/20 01:07
+ *  文件描述 :  权限管理：管理员控制器
+ *  历史记录 :  -----------------------
+ */
+namespace app\right_module\working_version\v1\controller;
+use think\Controller;
+use think\Request;
+
+class PageController extends Controller
+{
+    /**
+     * 名  称 : applyPreposition()
+     * 功  能 : 管理员登录前置接口
+     * 变  量 : --------------------------------------
+     * 输  入 : --------------------------------------
+     * 输  出 : --------------------------------------
+     * 创  建 : 2018/07/16 11:15
+     */
+    public function applyPreposition()
+    {
+
+    }
+
+    /**
+     * 名  称 : applyLogin()
+     * 功  能 : 显示管理员登录页面
+     * 变  量 : --------------------------------------
+     * 输  入 : --------------------------------------
+     * 输  出 : --------------------------------------
+     * 创  建 : 2018/07/16 11:15
+     */
+    public function applyLogin(Request $request)
+    {
+
+    }
+}
---
+++ route/page_route.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  page_route.php
+ *  创 建 者 :  Shi Guang Yu
+ *  创建日期 :  2018/07/20 16:27
+ *  文件描述 :  后台路由文件
+ *  历史记录 :  -----------------------
+ */
+
+// +---------------------------------------------
+// : 前台接口
+// +---------------------------------------------
+Route::group('v1/right_module/', function(){
+
+    // ---- 后台登录 ----
+
+    /**
+     * 路由名称：admin_preposition
+     * 传值方式：GET,
+     * 路由功能：管理员登录前置接口
+     */
+    Route::get(
+        'admin_preposition',
+        'right_module/v1.controller.PageController/adminPreposition'
+    );
+
+    /**
+     * 路由名称：admin_login
+     * 传值方式：GET,
+     * 路由功能：显示管理员登录页面
+     */
+    Route::get(
+        'admin_login',
+        'right_module/v1.controller.PageController/applyLogin'
+    );
+});
+
+// +---------------------------------------------
+// : 功能接口,需要权限验证接口
+// +---------------------------------------------
+Route::group('v1/right_module/', function(){
+
+})->middleware('Login_v1_IsToken');
+
+// +---------------------------------------------
+// : 后台接口,需要权限验证接口
+// +---------------------------------------------
+Route::group('v1/right_module/', function(){
+
+})->middleware('Right_v1_IsAdmin');
